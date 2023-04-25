abd51e5f24eb497c3ff8c3997748e426efd5bc6f
Be4gu <ioncristicristea@gmail.com>
2023-04-25 16:28:14 +0200

    Se crea el json para el swagger y para el yaml

--- utils/create-json-file.js
+++ utils/create-json-file.js
@@ -1,48 +1,53 @@
 const yaml = require('js-yaml')
 const fs = require('fs')
+import { HTMLTemplate } from './constants'
 
-export function createJson(listadoApis) {
-  let jsonObj = {}
-  listadoApis.forEach((item) => {
-    const obj = yaml.load(
-      fs.readFileSync(`src/pages/api/db/paths/${item.name}.yaml`, {
-        encoding: 'utf-8'
-      })
-    )
-    const prueba = {
-      ...jsonObj,
-      ...obj
-    }
-    jsonObj = prueba
+export function readFilesAndConvertToJson(fileNames, path) {
+  const result = {}
+
+  // if (!fileNames) return
+
+  fileNames.forEach((item) => {
+    const fileData = fs.readFileSync(`src/pages/api/db/${path}/${item}.yaml`, {
+      encoding: 'utf-8'
+    })
+    const yamlData = yaml.load(fileData)
+    Object.assign(result, yamlData)
   })
-  fs.writeFileSync('./datos.json', JSON.stringify(jsonObj), null, 2)
-  extractDefinitionsList(jsonObj)
-  //   return jsonObj
+  return result
 }
 
-function extractDefinitionsList(pathsList) {
-  let list = []
-  for (const key in pathsList) {
-    let c = {}
-    for (const statusCode in pathsList[key]) {
-      if (
-        statusCode === 'get' ||
-        statusCode === 'post' ||
-        statusCode === 'delete'
-      ) {
-        let x = pathsList[key][statusCode]
+export function removeDuplicateElements(arr) {
+  //Elimina los elementos duplicados de un array. Devuelve un array ya "limpio"
+  return [...new Set(arr)]
+}
 
-        for (const j in x['responses']) {
-          if (x['responses'][j]['schema'] !== undefined) {
-            if (x['responses'][j]['schema']['$ref']) {
-              list.push(x['responses'][j]['schema']['$ref'])
-            }
-          }
-        }
-      }
-    }
-  }
-  fs.writeFileSync('./datos2.json', JSON.stringify(list), null, 2)
-  const newList = [...new Set(list)]
-  console.log(newList.length)
+export function findMatches(str, regex) {
+  //Busca las coinidencias dentro de un string. Devuelve un array con todas las coincidencias.
+  const c = str.match(regex)
+  return c
+}
+
+export function modifyStringsBySubstring(arr) {
+  if (!arr) return
+
+  return arr.map((e) => {
+    return e.substring(e.lastIndexOf('/') + 1, e.lastIndexOf('"'))
+  })
+}
+
+export function generateHTMLFile(str, path) {
+  fs.writeFile(path, HTMLTemplate(str), function (err) {
+    if (err) throw err
+    console.log('Saved!')
+  })
+}
+
+export function getDefinitionsList(data, regex) {
+  const listDataDefinitions = findMatches(JSON.stringify(data), regex)
+  // console.log(listDataDefinitions)
+  const dataClear = modifyStringsBySubstring(listDataDefinitions)
+  const clearDuplicated = removeDuplicateElements(dataClear)
+  return clearDuplicated
+  // Enviar respuesta
 }
