81e2d25f9f78af3d89aa285aa8bc15fbe0fa26b4
Be4gu <ioncristicristea@gmail.com>
2024-03-22 09:42:24 +0100

    chechbox realizado

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -17,74 +17,75 @@ import {
 import { getPathName } from '@/utils/stringUtils'
 
 export default async function handler(req, res) {
-  try {
-    // Obtener el objeto que se va a escribir en el archivo JSON
-    const data = JSON.parse(req.body)
+  console.log(JSON.parse(req.body))
+  // try {
+  //   // Obtener el objeto que se va a escribir en el archivo JSON
+  //   const data = JSON.parse(req.body)
 
-    //Formatea el nombre de la carpeta en funcion de lo que se ha rellenado en el
-    const pathName = getPathName(data.name, data.entorno)
+  //   //Formatea el nombre de la carpeta en funcion de lo que se ha rellenado en el
+  //   const pathName = getPathName(data.name, data.entorno)
 
-    //Obtiene una lista de APIS
-    const newArr = data.apisList.map((item) => item.name)
+  //   //Obtiene una lista de APIS
+  //   const newArr = data.apisList.map((item) => item.name)
 
-    //Crea obtiene el JSON final necesario para la creacion de todos los ficheros salvo para el postman ya que tiene una estructura diferente.
-    const finalJson = getFinalJSON(newArr, data.entorno)
+  //   //Crea obtiene el JSON final necesario para la creacion de todos los ficheros salvo para el postman ya que tiene una estructura diferente.
+  //   const finalJson = getFinalJSON(newArr, data.entorno)
 
-    //Crea el dir de las carpetas necesarias para almacenar los fichero. El nombre de la carpeta lo recibe por parametro
-    crearCarpeta(pathName)
+  //   //Crea el dir de las carpetas necesarias para almacenar los fichero. El nombre de la carpeta lo recibe por parametro
+  //   crearCarpeta(pathName)
 
-    const archivoYAML = yaml.dump(finalJson) // Convierte el objeto JSON a YAML
-    //Crea el fichero .yaml
-    fs.writeFileSync(
-      `./entregables/${pathName}/01 - Yaml/APIs_Iberinform.yaml`,
-      archivoYAML
-    )
+  //   const archivoYAML = yaml.dump(finalJson) // Convierte el objeto JSON a YAML
+  //   //Crea el fichero .yaml
+  //   fs.writeFileSync(
+  //     `./entregables/${pathName}/01 - Yaml/APIs_Iberinform.yaml`,
+  //     archivoYAML
+  //   )
 
-    //Crea el fichero .html
-    generateHTMLFile(
-      JSON.stringify(finalJson),
-      `./entregables/${pathName}/02 - Swagger/APIs_Iberinform.html`
-    )
+  //   //Crea el fichero .html
+  //   generateHTMLFile(
+  //     JSON.stringify(finalJson),
+  //     `./entregables/${pathName}/02 - Swagger/APIs_Iberinform.html`
+  //   )
 
-    //Crea el fichero .txt
-    createTextFileKeys(`./entregables/${pathName}/keys.txt`, {
-      clientID: data.clientID,
-      clientSecret: data.clientSecret
-    })
+  //   //Crea el fichero .txt
+  //   createTextFileKeys(`./entregables/${pathName}/keys.txt`, {
+  //     clientID: data.clientID,
+  //     clientSecret: data.clientSecret
+  //   })
 
-    //Copia el pdf de su ruta orgen y lo deja en la ruta especificada.
-    copyPdfToDirectory(
-      `./entregables/${pathName}/04 - Documentacion/Iberinform_Ayuda_APIs_v1.0.pdf`
-    )
+  //   //Copia el pdf de su ruta orgen y lo deja en la ruta especificada.
+  //   copyPdfToDirectory(
+  //     `./entregables/${pathName}/04 - Documentacion/Iberinform_Ayuda_APIs_v1.0.pdf`
+  //   )
 
-    //Obtiene el objeto JSON necesario para la creacion del fichero postman
-    const postmanResult = createPostmanJson(
-      newArr,
-      data.clientSecret,
-      data.clientID
-    )
+  //   //Obtiene el objeto JSON necesario para la creacion del fichero postman
+  //   const postmanResult = createPostmanJson(
+  //     newArr,
+  //     data.clientSecret,
+  //     data.clientID
+  //   )
 
-    //Crea el fichero .json (El fichero para el postman)
-    fs.writeFileSync(
-      `./entregables/${pathName}/03 - Postman/IberinformApis.postman_collection.json`,
-      JSON.stringify(postmanResult, null, 2)
-    )
-    // require modules
-    zipCarpeta(pathName)
-      .then((fileContent) => {
-        // Aquí puedes hacer lo que necesites con el contenido del archivo ZIP
-        res.setHeader('Content-Type', 'application/zip')
-        res.setHeader('Content-Disposition', 'attachment; filename=archivo.zip')
-        res.setHeader('Content-Length', fileContent.length)
-        res.status(200).send(fileContent)
-      })
-      .catch((error) => {
-        console.error('Error al comprimir la carpeta:', error)
-      })
-  } catch (err) {
-    console.log(err)
-    res.status(500).json('Algo ha salido mal!')
-  }
+  //   //Crea el fichero .json (El fichero para el postman)
+  //   fs.writeFileSync(
+  //     `./entregables/${pathName}/03 - Postman/IberinformApis.postman_collection.json`,
+  //     JSON.stringify(postmanResult, null, 2)
+  //   )
+  //   // require modules
+  //   zipCarpeta(pathName)
+  //     .then((fileContent) => {
+  //       // Aquí puedes hacer lo que necesites con el contenido del archivo ZIP
+  //       res.setHeader('Content-Type', 'application/zip')
+  //       res.setHeader('Content-Disposition', 'attachment; filename=archivo.zip')
+  //       res.setHeader('Content-Length', fileContent.length)
+  //       res.status(200).send(fileContent)
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error al comprimir la carpeta:', error)
+  //     })
+  // } catch (err) {
+  //   console.log(err)
+  //   res.status(500).json('Algo ha salido mal!')
+  // }
 }
 
 // export async function GET() {
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import React, { useState } from 'react'
 import Select from 'react-select'
 import { Header } from '../../components/header'
 import { log } from 'util'
+import { list } from 'postcss'
+import { type } from 'os'
 // import { useSession } from 'next-auth/react'
 
 export default function Home() {
@@ -42,9 +44,7 @@ export default function Home() {
   const handleSeletAllApis = () => {
     if (!checked) {
       const newList = []
-      APIS_LIST.map((ictems, i) =>
-        newList.push({ name: ictems.value, id: uuidv4() })
-      )
+      APIS_LIST.map((items, i) => newList.push(items.value))
       setInputValues({
         ...inputValues,
         apisList: newList
@@ -137,22 +137,25 @@ export default function Home() {
     })
   })
 
-  const [originalArray, setOriginalArray] = useState([
-    'Elemento 1',
-    'Elemento 2',
-    'Elemento 3',
-    'Elemento 4'
-  ])
-  const [selectedItems, setSelectedItems] = useState([])
+  // const [selectedItems, setSelectedItems] = useState([])
 
   const handleToggleSelection = (item) => {
-    if (selectedItems.includes(item)) {
-      let aux = selectedItems.filter((selectedItem) => selectedItem !== item)
-      setSelectedItems(aux)
+    console.log(item)
+    if (inputValues.apisList.includes(item)) {
+      let aux = inputValues.apisList.filter(
+        (selectedItem) => selectedItem !== item
+      )
+      setInputValues({
+        ...inputValues,
+        apisList: aux
+      })
       console.log(aux)
-      console.log(selectedItems)
     } else {
-      setSelectedItems([...selectedItems, item])
+      setInputValues({
+        ...inputValues,
+        apisList: [...item]
+      })
+      console.log(inputValues.apisList)
     }
 
     // setInputValues({
@@ -161,155 +164,153 @@ export default function Home() {
     // })
   }
 
-  const handleAddToNewArray = () => {
+  const handleAddToNewArrayCheck = () => {
     // Aquí podrías hacer algo con los elementos seleccionados si es necesario
-    console.log('Elementos seleccionados:', selectedItems)
+    setSelectedItems(originalArray)
   }
 
+  // return (
+  //   <div>
+  //     <h1>Array Original</h1>
+  //     <ul>
+  //       {originalArray.map((item, index) => (
+  //         <li key={index}>
+  //           <label>
+  //             <input
+  //               type='checkbox'
+  //               className='accent-[#DC0028] w-4 h-4'
+  //               checked={selectedItems.includes(item)}
+  //               onChange={() => handleToggleSelection(item)}
+  //             />
+  //             {item}
+  //           </label>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //     {/* <button onClick={handleAddToNewArray}>
+  //       Agregar elementos seleccionados
+  //     </button> */}
+  //     <div>
+  //       {inputValues.apisList.map((item, index) => (
+  //         <li key={index}>{item}</li>
+  //       ))}
+  //     </div>
+  //     <input
+  //       id='checkbox-all-apis'
+  //       className='accent-[#DC0028] w-4 h-4'
+  //       type='checkbox'
+  //       // checked={checked}
+  //       onChange={handleSeletAllApis}
+  //     />
+  //   </div>
+  // )
   return (
-    <div>
-      <h1>Array Original</h1>
-      <ul>
-        {originalArray.map((item, index) => (
-          <li key={index}>
-            <label>
-              <input
-                type='checkbox'
-                checked={selectedItems.includes(item)}
-                onChange={() => handleToggleSelection(item)}
+    <>
+      <Header />
+
+      <h1 className='font-[Catalana-Bold] xl:text-4xl 2xl:text-5xl text-[#666666] text-center xl:mt-10 2xl:mt-12 2xl:mb-24 xl:mb-16'>
+        Generador de entregables API's
+      </h1>
+      <div className='w-full flex justify-start gap-20 px-10'>
+        <form
+          onSubmit={handleSubmit}
+          className=' w-80 flex xl:gap-y-0 2xl:gap-y-7 h-[558px] flex-wrap justify-between '
+        >
+          {/* Pinta todos los input segun la info que hay en la variable INPUT_INFO */}
+          {INPUT_INFO.map((info, index) => {
+            return (
+              <Input
+                changeValue={handleInputChange}
+                inputId={info.id}
+                labelText={info.labelText}
+                placeholder={info.placeholder}
+                type={info.type}
+                key={index}
               />
-              {item}
+            )
+          })}
+          <div className='font-[CatalanaSans-Regular] flex flex-col w-32'>
+            <label
+              htmlFor='searcherEnvironment'
+              className='text-[#828282] text-sm'
+            >
+              Entorno
             </label>
-          </li>
-        ))}
-      </ul>
-      <button onClick={handleAddToNewArray}>
-        Agregar elementos seleccionados
-      </button>
-      <div>
-        {inputValues.apisList.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
+            <Select
+              styles={customStyles(125)}
+              instanceId='searcherEnvironment'
+              options={[
+                { value: 'pro', label: 'Pro' },
+                { value: 'pre', label: 'Pre' }
+              ]}
+              defaultValue={[{ value: 'pre', label: 'Pre', isFixed: true }]}
+              onChange={(e) => {
+                handelChangeEntorno(e.value)
+              }}
+              isSearchable={false}
+            />
+          </div>
+
+          <div className='flex justify-between items-center'>
+            <label
+              htmlFor='checkbox-all-apis'
+              className='cursor-pointer font-[CatalanaSans-Regular] text-base text-[#828282] text-center mr-[6px]'
+            >
+              Sel. todo
+            </label>
+            <input
+              id='checkbox-all-apis'
+              className='accent-[#DC0028] w-4 h-4'
+              type='checkbox'
+              checked={checked}
+              onChange={handleSeletAllApis}
+            />
+          </div>
+          <button onClick={resteApiList} className='btn-primary' type='button'>
+            Res. todo
+          </button>
+          <div className='flex justify-center items-center w-full mt-3'>
+            <button
+              className='bg-[#DC0028] hover:bg-[#8E2A2B] py-4 px-5 rounded-[30px] uppercase font-[CatalanaSans-Medium] shrink-1 text-white leading-3'
+              type='submit'
+            >
+              Generar ficheros
+            </button>
+          </div>
+          <span>{err}</span>
+        </form>
+        <span
+          style={{ boxShadow: '20px 10px 6px -5px rgba(0,0,0,0.5)' }}
+          className='xl:h-[550px] 2xl:h-[600px] w-[3px] bg-[#82828280]'
+        ></span>
+        <div className='w-full '>
+          <h3 className='font-[Catalana-Bold] text-2xl text-[#666666] mb-5'>
+            Listado de API's
+          </h3>
+          <ul
+            typeof='none'
+            className='w-full  flex flex-wrap  font-[Catalana-Medium] text-[#666666]'
+          >
+            {APIS_LIST.map((item, index) => (
+              <li key={index}>
+                <label>
+                  <input
+                    type='checkbox'
+                    className='accent-[#DC0028] w-4 h-4'
+                    checked={inputValues.apisList.includes(item.value)}
+                    onChange={() => handleToggleSelection(item.value)}
+                  />
+                  {item.value}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
 // const { data: session } = useSession()
 // if (session) {
-// return (
-//   <>
-//     <Header />
-
-//     <h1 className='font-[Catalana-Bold] xl:text-4xl 2xl:text-5xl text-[#666666] text-center xl:mt-10 2xl:mt-12 2xl:mb-24 xl:mb-16'>
-//       Generador de entregables API's
-//     </h1>
-//     <div className='w-full flex justify-start gap-20 px-10'>
-//       <form
-//         onSubmit={handleSubmit}
-//         className=' w-80 flex xl:gap-y-0 2xl:gap-y-7 h-[558px] flex-wrap justify-between '
-//       >
-//         {/* Pinta todos los input segun la info que hay en la variable INPUT_INFO */}
-//         {INPUT_INFO.map((info, index) => {
-//           return (
-//             <Input
-//               changeValue={handleInputChange}
-//               inputId={info.id}
-//               labelText={info.labelText}
-//               placeholder={info.placeholder}
-//               type={info.type}
-//               key={index}
-//             />
-//           )
-//         })}
-//         <div className='font-[CatalanaSans-Regular] flex flex-col w-32'>
-//           <label
-//             htmlFor='searcherEnvironment'
-//             className='text-[#828282] text-sm'
-//           >
-//             Entorno
-//           </label>
-//           <Select
-//             styles={customStyles(125)}
-//             instanceId='searcherEnvironment'
-//             options={[
-//               { value: 'pro', label: 'Pro' },
-//               { value: 'pre', label: 'Pre' }
-//             ]}
-//             defaultValue={[{ value: 'pre', label: 'Pre', isFixed: true }]}
-//             onChange={(e) => {
-//               handelChangeEntorno(e.value)
-//             }}
-//             isSearchable={false}
-//           />
-//         </div>
-//         <div className='font-[CatalanaSans-Regular] flex flex-col w-80'>
-//           <label
-//             htmlFor='searcherEnvironment'
-//             className='text-[#828282] text-sm'
-//           >
-//             API's
-//           </label>
-//           <Select
-//             styles={customStyles(320)}
-//             instanceId='searcherApi'
-//             options={APIS_LIST}
-//             defaultValue={APIS_LIST[0]}
-//             onChange={(e) => {
-//               setApi(e.value)
-//             }}
-//           />
-//         </div>
-//         <div className='flex justify-between items-center'>
-//           <label
-//             htmlFor='checkbox-all-apis'
-//             className='cursor-pointer font-[CatalanaSans-Regular] text-base text-[#828282] text-center mr-[6px]'
-//           >
-//             Sel. todo
-//           </label>
-//           <input
-//             id='checkbox-all-apis'
-//             className='accent-[#DC0028] w-4 h-4'
-//             type='checkbox'
-//             checked={checked}
-//             onChange={handleSeletAllApis}
-//           />
-//         </div>
-//         <button onClick={resteApiList} className='btn-primary' type='button'>
-//           Res. todo
-//         </button>
-//         <div className='flex justify-center items-center w-full mt-3'>
-//           <button
-//             className='bg-[#DC0028] hover:bg-[#8E2A2B] py-4 px-5 rounded-[30px] uppercase font-[CatalanaSans-Medium] shrink-1 text-white leading-3'
-//             type='submit'
-//           >
-//             Generar ficheros
-//           </button>
-//         </div>
-//         <span>{err}</span>
-//       </form>
-//       <span
-//         style={{ boxShadow: '20px 10px 6px -5px rgba(0,0,0,0.5)' }}
-//         className='xl:h-[550px] 2xl:h-[600px] w-[3px] bg-[#82828280]'
-//       ></span>
-//       <div className='w-full '>
-//         <h3 className='font-[Catalana-Bold] text-2xl text-[#666666] mb-5'>
-//           Listado de API's
-//         </h3>
-//         <ul className='w-full list-disc flex flex-wrap  font-[Catalana-Medium] text-[#666666]'>
-//           {inputValues.apisList.map((apis, index) => {
-//             return (
-//               <CheckBox
-//                 text={apis.name}
-//                 id={apis.id}
-//                 onClick={handleDeleteApi}
-//                 key={index}
-//               />
-//             )
-//           })}
-//         </ul>
-//       </div>
-//     </div>
-//   </>
-// )
 // }
